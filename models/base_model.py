ef9bd869ed5bf9e6fbf26dfb2687d355f0e95c6c
Nico <lawani.gk@gmail.com>
2023-02-08 08:10:46 +0100

    updtate file_storage

--- models/base_model.py
+++ models/base_model.py
@@ -1,19 +1,41 @@
+#!/usr/bin/python3
+"""BaseModel module"""
 import uuid
-import datetime
+from datetime import datetime
+from models import storage
 
 
 class BaseModel:
     """defines all common attributes/methods for other classes"""
 
     id = str(uuid.uuid4())
-    created_at = datetime.datetime.today()
-    updated_at = datetime.datetime.today()
+    created_at = datetime.today()
+    updated_at = datetime.today()
+
+    def __init__(self, *args, **kwargs):
+        if kwargs:
+            for key, value in kwargs.items():
+                if key != '__class__':
+                    if key == 'created_at':
+                        value = datetime.fromisoformat(value)
+                        setattr(self, key, value)
+                        continue
+                    if key == 'updated_at':
+                        value = datetime.fromisoformat(value)
+                        setattr(self, key, value)
+                        continue
+                    setattr(self, key, value)
+        else:
+            self.id = str(uuid.uuid4())
+            self.created_at = datetime.today()
+            storage.new(self)
 
     def __str__(self):
         return "[{}] ({}) {}".format(self.__class__.__name__, self.id, self.__dict__)
 
     def save(self):
-        self.updated_at = datetime.datetime.today()
+        self.updated_at = datetime.today()
+        # storage.save()
 
     def to_dict(self):
         #        return {'my_number': self.my_number, 'name': self.__class__.__name__, id: self.id}
--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -14,11 +14,18 @@ class FileStorage:
 
     def new(self, obj):
         """sets in __objects the obj with key <obj class name>.id"""
-        __objects[f"{obj.__class__.__name__}.{obj.id}"] = obj
+        print("obj:", obj.to_dict())
+        self.__objects[f"{obj.__class__.__name__}.{obj.id}"] = obj.to_dict()
+        print("sob:", self.__objects)
 
     def save(self):
         """ serializes __objects to the JSON file (path: __file_path)"""
+        print("ob", self.__objects)
+
         with open(self.__file_path, 'w') as f:
+            """ if self.__objects == {}:
+                f.write('"{}"')
+            else: """
             json.dump(self.__objects, f)
 
     def reload(self):
