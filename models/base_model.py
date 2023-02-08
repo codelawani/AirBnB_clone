99a656bb9c02db2da16ae07d9996c925c6a1c4e6
Nico <lawani.gk@gmail.com>
2023-02-08 18:52:59 +0100

    modify basemodel

--- models/base_model.py
+++ models/base_model.py
@@ -8,9 +8,9 @@ from models import storage
 class BaseModel:
     """defines all common attributes/methods for other classes"""
 
-    id = str(uuid.uuid4())
+    """ id = str(uuid.uuid4())
     created_at = datetime.today()
-    updated_at = datetime.today()
+    updated_at = datetime.today() """
 
     def __init__(self, *args, **kwargs):
         if kwargs:
@@ -28,6 +28,7 @@ class BaseModel:
         else:
             self.id = str(uuid.uuid4())
             self.created_at = datetime.today()
+            self.updated_at = datetime.today()
             storage.new(self)
 
     def __str__(self):
