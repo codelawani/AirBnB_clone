f34a5258ee3f7aa3c96be5ef68769f3c80dbdf92
Nico <lawani.gk@gmail.com>
2023-02-10 17:14:38 +0100

    mistake

---
+++ files/review.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+from models.base_model import BaseModel
+"""A class module"""
+
+
+class Review(BaseModel):
+    """Creation of a class"""
+    place_id = ""
+    user_id = ""
+    text = ""
---
+++ models/state.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+from models.base_model import BaseModel
+"""A class module"""
+
+
+class State(BaseModel):
+    """Creation of a Class"""
+    name = ""
